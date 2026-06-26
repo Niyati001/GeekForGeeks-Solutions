5c9bac2e7dbbedf98ddfb29ab4dd2ba5ad6f9338
Niyati <ngupta6_be23@thapar.edu>
2026-06-26 18:20:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.cpp
@@ -0,0 +1,46 @@
+/* Structure of linked list Node
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* reverse(Node* head){
+          Node* prev= NULL;
+          Node* curr= head;
+          
+          while(curr){
+              Node* nextNode= curr-> next;
+              curr-> next= prev;
+              prev= curr;
+              curr= nextNode;
+          }
+          return prev;
+    }
+    
+    Node* addOne(Node* head) {
+       head= reverse(head);
+       Node* temp= head;
+       
+       int carry=1;
+       
+       while( temp && carry){
+           int sum= temp-> data + carry;
+           temp-> data= sum% 10;
+           carry= sum/10;
+           
+           if(carry && temp-> next== NULL){
+               temp-> next= new Node(0);
+           }
+           temp= temp-> next;
+       }
+       return reverse(head);
+    }
+};
