dbbaf8ea14aa8fb90b644a6af88b6f48eb169a61
Niyati <ngupta6_be23@thapar.edu>
2026-07-14 15:48:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shortest Path in Unweighted Graph/shortest-path-in-unweighted-graph.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    int shortestPath(int V, vector<vector<int>> &edges, int src, int dest) {
+        vector<vector<int>> adj(V);
+        
+        for(auto edge: edges){
+            int u= edge[0];
+            int v= edge[1];
+            
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        
+        queue<int> q;
+        vector<int> dist(V, 1e9);
+        dist[src]= 0;
+        q.push(src);
+        
+        while(!q.empty()){
+            int node= q.front();
+            q.pop();
+            
+            for(auto neigh: adj[node]){
+                if(dist[node]+1< dist[neigh]){
+                    dist[neigh]= dist[node]+1;
+                    q.push(neigh);                
+                }
+            }
+        }
+        if(dist[dest]== 1e9)
+            dist[dest]= -1;
+        
+        return dist[dest];
+    }
+};
