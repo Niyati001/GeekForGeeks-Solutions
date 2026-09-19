5d60228aa9aa2aaa6daf102ef4ff835ca9623e80
Niyati <ngupta6_be23@thapar.edu>
2026-09-19 17:23:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.cpp
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.cpp
@@ -14,24 +14,28 @@ public:
 class Solution {
   public:
     vector<int> leftView(Node *root) {
+        // code here
         vector<int> ans;
         if(root== NULL) return ans;
         
         queue<Node*> q;
-        
         q.push(root);
         
         while(!q.empty()){
             int size= q.size();
             
-            for(int i=0; i< size; i++){
-                Node* node= q.front();
+            for(int i=0; i<size; i++){
+                Node* curr= q.front();
                 q.pop();
                 
-                if(i==0) ans.push_back(node-> data);
-                
-                if(node-> left) q.push(node-> left);
-                if(node-> right) q.push(node-> right);
+                if(i==0)
+                    ans.push_back(curr-> data);
+                    
+                if(curr-> left) 
+                    q.push(curr-> left);
+                    
+                if(curr-> right)
+                    q.push(curr-> right);
             }
         }
         return ans;
