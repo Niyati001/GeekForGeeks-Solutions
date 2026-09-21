c680c5b97796df4f15e78b7b906f7f012b3ce147
Niyati <ngupta6_be23@thapar.edu>
2026-09-21 12:41:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,31 +1,36 @@
 class Solution {
   public:
-    bool dfs(int node, int parent, vector<bool>& vis, vector<vector<int>>& adj){
-        vis[node]= true;
-        for(auto adjNode: adj[node]){
-            if(!vis[adjNode]){
-                if(dfs(adjNode, node, vis, adj)== true) 
+    bool dfs(int node, int parent, vector<int>& vis, vector<vector<int>>& adj){
+        vis[node]=1;
+        
+        for(auto neig: adj[node]){
+            if(!vis[neig]){
+                if(dfs(neig, node, vis, adj))
                     return true;
             }
-            else if(adjNode!= parent) return true;
+            else if(neig!= parent)
+                return true;
         }
         return false;
     }
-  
+    
     bool isCycle(int V, vector<vector<int>>& edges) {
+        // Code here
         vector<vector<int>> adj(V);
-        for(auto &edge: edges){
+        vector<int>vis(V, 0);
+        
+        for(auto edge: edges){
             int u= edge[0];
             int v= edge[1];
             
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        vector<bool> vis (V, false);
         
         for(int i=0; i<V; i++){
             if(!vis[i]){
-                if(dfs(i, -1, vis, adj)== true) return true;
+                if(dfs(i, -1, vis, adj))
+                    return true;
             }
         }
         return false;
